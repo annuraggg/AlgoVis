44c1f155bb618206505fdd43454f5181cc2896a9
annuraggg <anuragsawant@duck.com>
2024-05-13 12:56:11 +0530

    Refactor Node component to dynamically set background color based on the provided color prop

--- src/routes/Pathfinding/Djikstras/Node.tsx
+++ src/routes/Pathfinding/Djikstras/Node.tsx
@@ -1,7 +1,13 @@
 const Node = ({ color }: { color: string }) => {
   return (
     <div
-      className={`border min-h-7 min-w-7 transition-all duration-30 0 bg-${color}-500 flex items-center justify-center`}
+      className={`border min-h-7 min-w-7 transition-all duration-30 0 flex items-center justify-center
+      ${color === "red" && "bg-red-500"}
+      ${color === "green" && "bg-green-500"}
+      ${color === "blue" && "bg-blue-500"}
+      ${color === "gray" && "bg-gray-500"}
+      ${color === "yellow" && "bg-yellow-500"}
+      `}
     ></div>
   );
 };
